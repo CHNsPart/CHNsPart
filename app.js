2e5ef3524ec7d942cebdf9b0212f9378cfc319fd
Touhidul Islam Chayan <imchn24@gmail.com>
2020-10-03 05:26:13 +0600

    port fixed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 const { response } = require("express");
 const { render } = require("ejs");
 var app = express();
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
