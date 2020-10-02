ca6b02300e08bc7910039f58a5546419d7d4b7eb
Touhidul Islam Chayan <imchn24@gmail.com>
2020-10-03 05:17:58 +0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require("express");
+const { response } = require("express");
+const { render } = require("ejs");
+var app = express();
+const port = process.env.port || 8080;
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+app.get("/designs", function (req, res) {
+  res.render("designs");
+});
+app.get("/projects", function (req, res) {
+  res.render("projects");
+});
+app.get("/aboutme", function (req, res) {
+  res.render("aboutme");
+});
+app.get("/contact", function (req, res) {
+  res.render("contact");
+});
+
+app.listen(port, () => console.log("Started.............."));
---
+++ public/js/play.js
@@ -0,0 +1,8 @@
+let mail = document.querySelector("#mail");
+let fbname = document.querySelector(".name");
+mail.addEventListener("click", () => {
+  window.open("https://mailto:imchn24@gmail.com", "_blank");
+});
+fbname.addEventListener("click", () => {
+  window.open("https://facebook.com/CHNsPart", "_blank");
+});
